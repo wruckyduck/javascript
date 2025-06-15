af573bd9bab3daca8787eb39102b7dd0be7c189c
Harry Wruck <harrywruck22@gmail.com>
2025-06-15 11:50:06 +1000

    Updating lesson1 file with new changes

--- lesson1.js
+++ lesson1.js
@@ -1,3 +1,5 @@
+/*
+
 const string = "The revolution will not be televised.";
 console.log(string);
 
@@ -8,7 +10,28 @@ console.log(single);
 console.log(double);
 console.log(backtick);
 
-const badQuotes = 'This is not allowed!';
+*/
+
+// const badQuotes = 'This is not allowed!';
+
+// const name = "Chris"; 
+// const greeting = `Hello, ${name}`; 
+// console.log(greeting); // "Hello, Chris"
+
+// const one = "Hello, ";
+// const two = "how are you?"; 
+// const joined = `${one}${two}`;
+// console.log(joined);
+
+let greeting = "happy"; 
+let name = "dog";
+console.log(greeting + ', ' + name); //"Hello, Chris"
+
+const song = "Fight the Youth"; 
+const score = 9; 
+const highestScore = 10; 
+const output = `I like the song ${song}. I gave it a score of ${ (score / highestScore) * 100}%`
+console.log(output)
 
 
 
