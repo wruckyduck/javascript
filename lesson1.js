f1e5b79ae23d056d74be021dcfc001beecc4be89
Harry Wruck <harrywruck22@gmail.com>
2025-06-15 11:19:39 +1000

    First lesson in javascript

---
+++ lesson1.js
@@ -0,0 +1,14 @@
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const single = 'Single quotes'
+const double = "Double quotes"
+const backtick = `Backtick`; 
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+const badQuotes = 'This is not allowed!';
+
+
+
